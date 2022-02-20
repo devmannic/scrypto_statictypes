d371a43f4ea013593c2ae5f2ccba0671fdf31485
devmannic <devmannic@users.noreply.github.com>
2022-02-20 05:46:52 +0000

    deploy: b09788beb474fa7edfb622ba182e5e6912ecba40

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["scrypto_statictypes"] = {"name":"","files":["bucketof.rs","bucketrefof.rs","internal.rs","lib.rs","macros.rs","prelude.rs","resourceof.rs","runtime.rs","vaultof.rs"]};
+sourcesIndex["scrypto_statictypes"] = {"name":"","files":["bucketof.rs","bucketrefof.rs","exts.rs","internal.rs","lib.rs","macros.rs","prelude.rs","resourceof.rs","runtime.rs","vaultof.rs"]};
 createSourceSidebar();
